21be78d35eeda4badb5b65c89f33b01ef3a896db
Ramil Mufazzalov <aoho92@gmail.com>
2023-04-04 18:02:01 +0500

    удаление, запись id и времени

--- notes.py
+++ notes.py
@@ -4,22 +4,47 @@ from tkinter import filedialog
 from tkinter import messagebox
 from tkinter.messagebox import showinfo
 import os
+import uuid
+import time
+from os.path import getctime
+from datetime import datetime
 file_name = None
 
 
 def new_file():
     global file_name
+    text.pack()  # вставляем текстовое окно
     file_name = 'Без названия'
     text.delete('1.0', END)  # очищаем текстовое окно
+    noteid = 'note ID: ' + str(uuid.uuid4()) + '\n'
+    text.insert('1.0', noteid)
 
 
 def save_as():
     out = asksaveasfile(mode='w', defaultextension='.json')
     data = text.get('1.0', END)
+    data = str(data)
     try:
-        out.write(data.rstrip())
+        # out.write(data.rstrip())
+        out.write(data)
     except Exception:
         messagebox.showerror('Не получилось сохранить файл')
+
+
+def open_file():
+    global file_name
+    inp = askopenfile(mode='r')
+    if inp is None:
+        return
+        file_name = inp.name
+    data = inp.read()
+    text.pack()
+    text.delete('1.0', END)
+    text.insert('1.0', data)
+    # filedate = os.path.getctime(inp.name)
+    filedate = 'Дата изменения файла: ' + \
+        str(time.ctime(os.path.getmtime(inp.name))) + '\n'
+    text.insert('1.0', filedate)
 
 
 def delete_file():
@@ -31,24 +56,11 @@ def delete_file():
         messagebox.showerror('Не получилось удалить файл')
 
 
-def open_file():
-    global file_name
-    inp = askopenfile(mode='r')
-    if inp is None:
-        return
-        file_name = inp.name
-    data = inp.read()
-    text.delete('1.0', END)
-    text.insert('1.0', data)
-
-
 root = Tk()  # окно заметок
 root.title('Заметки')
 root.geometry('400x400')
 
 text = Text(root, width=400, height=400)
-text.pack()  # вставляем текстовое окно
-
 menu_bar = Menu(root)
 file_menu = Menu(menu_bar)
 
